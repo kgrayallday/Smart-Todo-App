c81ae140044868fd2500f4a37df133c5b893f3ce
kyl graym <kylemaxgray@gmail.com>
2021-09-18 21:25:51 +0000

    create categories lookup/reference table

---
+++ db/schema/02_categories.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS categories CASCADE;
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO categories (name)
+VALUES('uncategorized', 'Books', 'Films', 'Foods', 'Products' );
