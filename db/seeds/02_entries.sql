79b413f3acb7f72745595f75641ecbb33c2281eb
kyl graym <kylemaxgray@gmail.com>
2021-09-18 18:45:25 +0000

    add seeds for todo entries for testing

---
+++ db/seeds/02_entries.sql
@@ -0,0 +1,41 @@
+INSERT INTO entries (
+  catergory_id,
+  user_id,
+  created_date,
+  due_date,
+  completed_date,
+  current_status,
+  name
+  description)
+  VALUES(
+    1,
+    1,
+    '2021-09-17 12:30:45.25+00',
+    null,
+    '2021-09-24 18:30:45.25+00',
+    0,
+    'Have Midterm Complete',
+    'needs to be complete my 10am Friday morning'
+  );
+
+  VALUES(
+    2,
+    2,
+    '2021-09-18 14:11:23.20+00',
+    null,
+    '2021-09-20 14:11:23.20+00',
+    0,
+    'Watch Hary Potter and Lord of the Rings at the same time',
+    'have each playing on different screens'
+  );
+
+    VALUES(
+    3,
+    3,
+    '2021-09-18 16:31:11.11+00',
+    null,
+    '2021-09-20 14:11:23.20+00',
+    0,
+    'pick up avocados from the grocery store on the way home',
+    'need two green and two ripe'
+  );
