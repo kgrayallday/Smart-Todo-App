f6ee284a8ae6d02d4bc23d15de7a4f32d592baf8
kyl graym <kylemaxgray@gmail.com>
2021-09-18 18:19:57 +0000

    update user seed info for testing

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- '$2a$10$Te7QgBHhFO9.Gn6dgv1JVurX3fHnmboh4QtkKI/CpkV4.hme2jIqC' --password: lighthouse
+INSERT INTO users (name, email, password) VALUES ('Kyle', 'kyle@todo.com', '$2a$10$Te7QgBHhFO9.Gn6dgv1JVurX3fHnmboh4QtkKI/CpkV4.hme2jIqC');
+INSERT INTO users (name, email, password) VALUES ('Tommy', 'tommy@todo.com', '$2a$10$Te7QgBHhFO9.Gn6dgv1JVurX3fHnmboh4QtkKI/CpkV4.hme2jIqC');
+INSERT INTO users (name, email, password) VALUES ('Michael', 'michael@todo.com', '$2a$10$Te7QgBHhFO9.Gn6dgv1JVurX3fHnmboh4QtkKI/CpkV4.hme2jIqC');
