942c8732bbc7a51daeba81630574b9c350ad778e
vagrant <rpg7ofdeath@gmail.com>
2021-09-19 21:03:56 +0000

    moved hard coded scripts to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,4 @@
+/*
 $(() => {
   $.ajax({
     method: "GET",
@@ -7,4 +8,51 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+});
+*/
+
+$(document).ready(() => {
+  //header back btn
+  $('#back').click(() => {
+    window.history.back();
+  })
+  //home btns
+  $('#multimedia').click(() => {
+    window.location.href='/category/multimedia';
+  });
+  $('#eating').click(() => {
+    window.location.href='/category/eating';
+  });
+  $('#reading').click(() => {
+    window.location.href='/category/reading';
+  });
+  $('#buying').click(() => {
+    window.location.href='/category/buying';
+  });
+  $('#misc').click(() => {
+    window.location.href='/category/misc';
+  });
+  //profile btn
+  $('#profile-edit').click(() => {
+    window.location.href='/profile/edit';
+  });
+  //footer btn
+  $('#footer-home').click(() => {
+    window.location.href='/home';
+  });
+  $('#footer-multimedia').click(() => {
+    window.location.href='/category/multimedia';
+  });
+  $('#footer-eating').click(() => {
+    window.location.href='/category/eating';
+  });
+  $('#footer-reading').click(() => {
+    window.location.href='/category/reading';
+  });
+  $('#footer-buying').click(() => {
+    window.location.href='/category/buying';
+  });
+  $('#footer-profile').click(() => {
+    window.location.href='/profile';
+  });
 });
