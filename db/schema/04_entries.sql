e76226e360955a654cb1edd01ae9a5c3f35d9c30
kyl graym <kylemaxgray@gmail.com>
2021-09-19 22:46:42 +0000

    update data type from date to timestamp

--- db/schema/04_entries.sql
+++ db/schema/04_entries.sql
@@ -6,7 +6,7 @@ CREATE TABLE entries (
   catergory_id SMALLINT NOT NULL,
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  created_date DATE DEFAULT CURRENT_TIMESTAMP(2),
+  created_date timestamp DEFAULT CURRENT_TIMESTAMP(2),
   due_date DATE,
   completed_date DATE,
   statuses_id SMALLINT NOT NULL REFERENCES statuses(id) ON DELETE CASCADE DEFAULT 1
