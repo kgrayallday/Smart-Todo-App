070cf273552aa04d17f6e51a60f908415973fcf2
kyl graym <kylemaxgray@gmail.com>
2021-09-19 21:45:12 +0000

    update entries schema, some data seems to have been lost in branch merge

--- db/schema/03_entries.sql
+++ db/schema/03_entries.sql
@@ -1,14 +1,13 @@
 DROP TABLE IF EXISTS entries CASCADE;
--- CREATE TYPE status AS ENUM('active', 'delted', 'finished');
 
 CREATE TABLE entries (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  catergory_id SMALLINT, -- opting for lookup tables over enum
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  catergory_id SMALLINT NOT NULL,
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  created_date DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(2),
+  created_date DATE DEFAULT CURRENT_TIMESTAMP(2),
   due_date DATE,
   completed_date DATE,
-  current_status SMALLINT NOT NULL DEFAULT 0
+  statuses_id SMALLINT NOT NULL REFERENCES statuses(id) ON DELETE CASCADE DEFAULT 1
 );
