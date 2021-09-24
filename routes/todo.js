417d4a161d9ea4c5b066a2ccd60086df53f3e6f2
Mai Duo Wu <wmaiduo@gmail.com>
2021-09-24 00:22:19 +0000

    updated todo and fetchDatatypes so it uses both action verb and Wolfram Alpha API to find the data type

--- routes/todo.js
+++ routes/todo.js
@@ -7,22 +7,14 @@ module.exports = (db) => {
   router.post("/", (req, res) => {
     const title = req.body.new_task;
     const userId = req.session.userID || 1;
-    let category = 5;
-    if (title.includes("read")) {
-      category = 2;
-    } else if (title.includes("watch")) {
-      category = 3;
-    } else if (title.includes("eat")) {
-      category = 4;
-    } else if (!title.includes("buy")) {
-      category = 1;
-    }
-    newEntry({ userId, title, name: null, category_id: category }, db)
-      .then((response) => {
-        console.log(response);
-        res.redirect("/");
-      })
-      .catch((error) => console.log(error));
+    fetchDatatypes(title).then(category =>
+      newEntry({ userId, title, name: null, category_id: category }, db)
+        .then((response) => {
+          console.log(response);
+          res.redirect("/");
+        })
+        .catch((error) => console.log(error))
+    );
   });
   return router;
 };
